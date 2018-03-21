d6fa4c014b181ca2cbcc979f73cd1aa1e359b64c
Ekoodi-Jouni <jouni.k.kononen@gmail.com>
2018-03-21 14:21:26 +0200

    Classes generated from Database by using EF Core Tool

---
+++ ConsoleApp/PersonTest/PersonTest/Config/PersontestdbContext.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace PersonTest.Model
+{
+    public partial class PersontestdbContext : DbContext
+    {
+        public virtual DbSet<Person> Person { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer(@"Data Source=DKO-S010A-016\SQLEXPRESS;Initial Catalog=PersonTestDB;Integrated Security=True");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Person>(entity =>
+            {
+                entity.Property(e => e.City).IsUnicode(false);
+
+                entity.Property(e => e.FirstName).IsUnicode(false);
+
+                entity.Property(e => e.LastName).IsUnicode(false);
+            });
+        }
+    }
+}

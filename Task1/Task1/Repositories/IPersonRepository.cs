1e218aa1bca0a1e1812d8bcc04d8067e2762dcbc
Ekoodi-Jouni <jouni.k.kononen@gmail.com>
2018-03-29 09:22:23 +0300

    IPersonRepository.cs added to GIT

---
+++ Task1/Task1/Repositories/IPersonRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Task1.Model;
+
+namespace Task1.Repositories
+{
+    interface IPersonRepository
+    {
+        /// <summary>
+        /// Tänne kuvataan metodit, joita PersonRepositorin tulee toteuttaa
+        /// Paluuarvo ja MetodinNimi(mahdolliset parametrit)
+        /// </summary>
+        ///     
+            
+        List<Person> Get();
+        Person GetPersonById(int id);
+        void Create(Person person);
+        void Update(int id, Person person);
+        void Delete(int id);
+        
+
+    }
+}

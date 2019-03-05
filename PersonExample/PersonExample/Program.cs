4a7012f473616534a5f3c242dcfb4f6173b4b6c1
e310905-saimia <jouni.k.kononen@gmail.com>
2019-03-05 13:50:08 +0200

    Person Repository defined ReadByCity, ReadById implemented Program.cs has done tester for ReadByCity and ReadById

--- PersonExample/PersonExample/Program.cs
+++ PersonExample/PersonExample/Program.cs
@@ -1,12 +1,36 @@
 ﻿using System;
+using System.Runtime.InteropServices;
+using PersonExample.Repositories;
 
 namespace PersonExample
 {
     class Program
     {
+        private static readonly PersonRepository _personRepository = new PersonRepository();
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Testing database Read
+            //ReadByCity();
+            ReadById();
+
+            
+        }
+
+        static void ReadByCity()
+        {
+            var persons = _personRepository.ReadByCity("Juuka");
+
+            foreach (var p in persons)
+            {
+                Console.WriteLine($"{p.Id} {p.FirstName} {p.LastName} {p.City}");
+            }
+        }
+
+        static void ReadById()
+        {
+            var person = _personRepository.ReadById(1);
+
+            Console.WriteLine($"{person.Id} {person.FirstName} {person.LastName} {person.City}");
         }
     }
 }
---
+++ PersonExample/PersonExample/Repositories/IPersonRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using PersonExample.Models;
+
+namespace PersonExample.Repositories
+{
+    public interface IPersonRepository
+    {
+        //CRUD
+        void Create(Person person);
+        List<Person> ReadByCity(string city);
+        Person ReadById(long id);
+        void Update(long id, Person person);
+        void Delete(long id);
+    }
+}
